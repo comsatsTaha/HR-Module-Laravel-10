15e0e9dc825118ea2e7999e83f90c5027ec235f0
comsatsTaha <comsats.taha@gmail.com>
2024-03-01 09:43:10 +0500

    change leave status

--- resources/views/form/leaves.blade.php
+++ resources/views/form/leaves.blade.php
@@ -154,28 +154,7 @@
                                                     </div>
                                                 </div>
                                             </td>
-                                            <div class="modal custom-modal fade" id="approve_leave{{$items->id}}" role="dialog">
-                                                <div class="modal-dialog modal-dialog-centered">
-                                                    <div class="modal-content">
-                                                        <div class="modal-body">
-                                                            <div class="form-header">
-                                                                <h3>Leave Approve</h3>
-                                                                <p>Are you sure want to approve for this leave?</p>
-                                                            </div>
-                                                            <div class="modal-btn delete-action">
-                                                                <div class="row">
-                                                                    <div class="col-6">
-                                                                        <a data-dismiss="modal" class="btn btn-primary continue-btn" onclick="statuschange('Approved',{{$items->id}})">Approve</a>
-                                                                    </div>
-                                                                    <div class="col-6">
-                                                                        <a data-dismiss="modal" class="btn btn-primary cancel-btn" onclick="statuschange('Disapproved',{{$items->id}})">Decline</a>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
+                                            @include('form.modal.statusmodal')
                                             <td class="text-right">
                                                 <div class="dropdown dropdown-action">
                                                     <a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="material-icons">more_vert</i></a>
@@ -344,49 +323,7 @@
     <!-- /Page Wrapper -->
     @section('script')
 
-    <script>
-        function statuschange(status, leaveId) {
-            $.ajaxSetup({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                }
-            });
-
-            $.ajax({
-                url: '{{ route('changeLeaveStatus') }}',
-                type: 'POST',
-                dataType: 'json',
-                data: {
-                    status: status,
-                    leave_id: leaveId 
-                },
-                success: function(response) {
-                    console.log(response);
-                    $('#labelstatus').text(response.status);
-
-                },
-                error: function(xhr, status, error) {
-                    console.error(xhr.responseText);
-                }
-            });
-    }
-
-        $(document).ready(function() {
-          
-            function changeStatus(status) {
-             
-            }
     
-            // Attach click event handlers using jQuery
-            $('.continue-btn').on('click', function() {
-                changeStatus('Approved');
-            });
-    
-            $('.cancel-btn').on('click', function() {
-                changeStatus('Declined');
-            });
-        });
-    </script>
     <script>
         document.getElementById("year").innerHTML = new Date().getFullYear();
     </script>
---
+++ resources/views/form/modal/statusmodal.blade.php
@@ -0,0 +1,67 @@
+<div class="modal custom-modal fade" id="approve_leave{{$items->id}}" role="dialog">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-body">
+                <div class="form-header">
+                    <h3>Leave Approve</h3>
+                    <p>Are you sure want to approve for this leave?</p>
+                </div>
+                <div class="modal-btn delete-action">
+                    <div class="row">
+                        <div class="col-6">
+                            <a data-dismiss="modal" class="btn btn-primary continue-btn" onclick="statuschange('Approved',{{$items->id}})">Approve</a>
+                        </div>
+                        <div class="col-6">
+                            <a data-dismiss="modal" class="btn btn-primary cancel-btn" onclick="statuschange('Disapproved',{{$items->id}})">Decline</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script>
+    function statuschange(status, leaveId) {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $.ajax({
+            url: '{{ route('changeLeaveStatus') }}',
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                status: status,
+                leave_id: leaveId 
+            },
+            success: function(response) {
+                console.log(response);
+                $('#labelstatus').text(response.status);
+
+            },
+            error: function(xhr, status, error) {
+                console.error(xhr.responseText);
+            }
+        });
+}
+
+    $(document).ready(function() {
+      
+        function changeStatus(status) {
+         
+        }
+
+        // Attach click event handlers using jQuery
+        $('.continue-btn').on('click', function() {
+            changeStatus('Approved');
+        });
+
+        $('.cancel-btn').on('click', function() {
+            changeStatus('Declined');
+        });
+    });
+</script>
